80cbc337afa17acf4f15e72c098461fc12dc0234
Rylan Dmello <mail@rylan.coffee>
2020-10-14 18:38:52 -0400

    Update graphics

--- run.sh
+++ run.sh
@@ -6,10 +6,10 @@ set -eu
 finish() { stty echo; }
 trap finish EXIT
 
-# Default dimensions to 40x12 if not provided by user.
-WIDTH=${1:-40}
+# Default dimensions to 41x12 if not provided by user.
+WIDTH=${1:-41}
 HEIGHT=${2:-12}
-DIFFICULTY=${3:-70} # Difficulty is between 1-100, default to 70.
+DIFFICULTY=${3:-74} # Difficulty is between 1-100, default to 74.
 
 # Disable echo so that input chars are not echoed in the terminal
 stty -echo
