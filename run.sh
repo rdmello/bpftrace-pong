378119c86b23443227c5b3adc09f77dcbb72fa46
Rylan Dmello <mail@rylan.coffee>
2020-10-14 00:13:36 -0400

    Add some AI jitter

--- run.sh
+++ run.sh
@@ -6,9 +6,11 @@ set -eu
 finish() { stty echo; }
 trap finish EXIT
 
+# Default dimensions to 40x12 if not provided by user.
 WIDTH=${1:-40}
 HEIGHT=${2:-12}
+DIFFICULTY=${3:-70} # Difficulty is between 1-100, default to 70.
 
 # Disable echo so that input chars are not echoed in the terminal
 stty -echo
-bpftrace pong.bt $WIDTH $HEIGHT
+bpftrace pong.bt $WIDTH $HEIGHT $DIFFICULTY
